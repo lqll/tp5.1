5a1fbd5a71ede47b4962d62e38105851bc072a53
chengguoyong <845327689@qq.com>
2018-11-20 16:18:11 +0800

    接口测试

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,20 @@
 <?php
 namespace app\index\controller;
 
+use think\App;
 use think\Controller;
 
 class Index extends Controller
 {
+    public function __construct(App $app = null)
+    {
+        parent::__construct($app);
+        header("Access-Control-Allow-Origin:*");
+        header('Access-Control-Allow-Methods:GET, POST, PATCH, PUT, DELETE');
+        header('Access-Control-Allow-Headers:Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With');
+        header('Access-Control-Allow-Credentials:true');
+    }
+
     public function index()
     {
         return json(['data'=>1]);
